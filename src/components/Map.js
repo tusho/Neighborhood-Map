042d18284a4b79fcd25d197656b3323fd28ec44b
Armin Kuhestani <armin_kuhestani@apple.com>
2019-01-30 12:13:34 +1100

    Added infowindow attached to each marker

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
-import {Map, Marker, GoogleApiWrapper, mapTypeControlOptions} from 'google-maps-react';
+import {Map, Marker, GoogleApiWrapper, InfoWindow} from 'google-maps-react';
 import {Venues}  from './Venues'
 
   export class MapContainer extends Component {
    
     state = {
-      mapTypeControlOptions: false,
+      selectedVenue: this.props,
+      activeMarker: this.marker,
+      showingInfoWindow: true
     }
+
+    onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+    onClose = props => {
+      if (this.state.showingInfoWindow) {
+        this.setState({
+          showingInfoWindow: false,
+          activeMarker: null
+        });
+      }
+    };
 
     render() {
 
@@ -23,12 +41,22 @@ import {Venues}  from './Venues'
                 zoom={15}>
                 {Venues.map((venue) => (
                   <Marker
+                  onClick={this.onMarkerClick}
+                  key={venue.name}
                   name={venue.name}
                   position={venue.position} 
                   />
                 ))}
+                <InfoWindow
+                  marker={this.state.activeMarker}
+                  visible={this.state.showingInfoWindow}
+                  onClose={this.onClose}
+                >
+                  <div>
+                    <h4>{this.state.selectedVenue.name}</h4>
+                  </div>
+                </InfoWindow>
             </Map>
-
         </div>
      );
      }
