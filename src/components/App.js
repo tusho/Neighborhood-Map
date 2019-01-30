84199005c6c6c43c8cf8a21816da237659f73ec7
Armin Kuhestani <armin_kuhestani@apple.com>
2019-01-30 14:37:23 +1100

    Adjusted burger menu width

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Menu noOverlay width={ '20%' }>
+        <Menu noOverlay width={ '320px' }>
           <MenuBar />
         </Menu>
         <MapContainer />
